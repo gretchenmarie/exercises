a616b54f4d5a7e36a6d43512f6cd6e3f91f2aeef
gretchenmarie <gretchenmward@gmail.com>
2018-07-13 12:56:16 -0500

    changes

--- 07-13/main.js
+++ 07-13/main.js
@@ -14,20 +14,69 @@
 
 
 
-function go(direction,speed);
-{
+//function go (direction,speed); 
+//{
     //console.log (`The car is moving ${direction}, at ${speed} mph.`);
     //console.log ("The car is moving" + direction ", at" + speed " mph.");
-if (speed >75){
-    console.log(`The car is moving ${direction}, at ${speed} mph. Slow Down!! `);
+//if (speed >75){
+//    console.log(`The car is moving ${direction}, at ${speed} mph. Slow Down!! `);
     
-} else {
-    console.log(`The car is moving ${direction}, at ${speed} mph. You're Good!`);
-}
+//} else {
+  //  console.log(`The car is moving ${direction}, at ${speed} mph. You're Good!`);
+//}
 
 
    // if (speed > 82)
     //{ alert ("Slow down!");
     //}
+//}
+//go("forward","25");
+
+
+
+//Given the following set of data about chicken birth rates, you need to filter out the objects
+// where the birth rate value is greater
+// than 15 per 1000 chickens for that year. Store the good years in a new array named `bestYears`.
+
+const bestYears = [];
+const birthRates = [
+    { year: 1969, birthRate: 13}, 
+    { year: 1970, birthRate: 16}, 
+    { year: 1971, birthRate: 15}, 
+    { year: 1972, birthRate: 11}, 
+    { year: 1973, birthRate: 18}, 
+    { year: 1974, birthRate: 17}, 
+    { year: 1975, birthRate: 9}
+  ];
+
+//for (let i = 0; i < birthRates.length; i++) {
+ //   const currentYear = birthRates[i];
+//if (currentyear.birthrate > 15)
+ //   { bestYears.push (currentyear)}
+    
+//}
+//birthRates.forEach (function(currentyear)
+//)
+
+//for (let currentYear of birthRates){
+//if (currentYear.birthRate > 15){
+//   bestYears.push(currentYear)
+//}
+
+
+//console.log (bestYears)
+
+
+
+const politician ={
+    platform:{
+        money: [ "I like it", "I have lots of it", "I do not want other people to have it"],
+        health: "I'm against it"
+    },
+    disrict: "Michigan 13"
 }
-go("forward","25");
+console.log(politician.platform.money[1]) 
+
+for (const policy of politician.platform.money [2]){
+    console.log(policy) I like it, I have lots of it, I do not want other people to have it
+}
